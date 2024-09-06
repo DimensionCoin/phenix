7e460936d5d33d53f87e0ca5e235faf87dddc3fb
DimensionCoin <nick.vassallo97@gmail.com>
2024-09-06 19:42:09 -0400

    hell

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    // Add other PWA-specific options here
-  },
+const nextConfig = {
   reactStrictMode: true,
-  // Add other Next.js config options here
   images: {
-    domains: ["example.com", "another-domain.com"], // Example for allowed image domains
+    domains: [ "https://tokens.jup.ag/token"], // Example for allowed image domains
   },
-});
+  // Add other Next.js config options here
+};
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // Add other PWA-specific options here
+})(nextConfig);
